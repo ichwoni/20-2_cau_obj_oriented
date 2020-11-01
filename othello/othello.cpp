1f30df2b4f2076ffa9064bdcf5656a168966b8cf
ichwoni <codnjs0397@naver.com>
2020-11-01 23:59:14 +0900

    1101

---
+++ othello/othello.cpp
@@ -0,0 +1,134 @@
+#include <bangtal.h>
+using namespace bangtal;
+
+ObjectPtr board[8][8];
+
+enum  class State {
+	BLANK,
+	POSSIBLE,
+	BLACK,
+	WHITE
+};
+State board_state[8][8];
+
+enum class Turn {
+	BLACK,
+	WHITE
+};
+Turn turn = Turn::BLACK;
+
+
+void setState(int x, int y, State state)
+{
+	switch (state) {
+	case State::BLANK: board[y][x]->setImage("Images/blank.png"); break;
+	case State::POSSIBLE: board[y][x]->setImage(turn == Turn::BLACK ? "Images/black possible.png" : "Images/white possible.png"); break;
+	case State::BLACK: board[y][x]->setImage("Images/black.png"); break;
+	case State::WHITE: board[y][x]->setImage("Images/white.png"); break;
+	}
+	board_state[y][x] = state;
+}
+
+bool checkPossible(int x, int y, int dx, int dy)
+{
+	State self = turn == Turn::BLACK ? State::BLACK : State::WHITE;
+	State other = turn == Turn::BLACK ? State::WHITE : State::BLACK;
+
+	bool possible = false;
+
+	for (x += dx, y += dy; x >= 0 && x < 8 && y >= 0 && y < 8; x += dx, y += dy) {
+		if (board_state[y][x] == other) {
+			possible = true;
+		}
+		else if (board_state[y][x] == self) {
+			return possible;
+		}
+		else {
+			return false;
+		}
+	}
+
+	return false;
+}
+
+bool checkPossible(int x, int y) {
+
+	if (board_state[x][y] == State::BLACK)
+		return false;
+	if (board_state[x][y] == State::WHITE)
+		return false;
+	setState(x, y, State::BLANK);
+
+	int delta[8][2] = {
+		{0,1},
+		{1,1},
+		{1,0},
+		{1,-1},
+		{0,-1},
+		{-1,-1},
+		{-1,0},
+		{-1,-1}
+	};
+
+	bool possible = false;
+	for (auto d : delta) {
+		if (checkPossible(x, y, d[0], d[1]))
+			possible = true;
+	}
+
+
+	return possible;
+}
+
+//8*8의 모든 위치에 대해 놓여질 수 있으면 상태를 POSSIBLE로
+void setPossible() {
+	for (int y = 0; y < 8; y++) {
+		for (int x = 0; x < 8; x++) {
+			if (checkPossible(x, y)) {
+				setState(x, y, State::POSSIBLE);
+			}
+		}
+	}
+}
+
+
+int main() {
+	setGameOption(GameOption::GAME_OPTION_INVENTORY_BUTTON, false);
+	setGameOption(GameOption::GAME_OPTION_MESSAGE_BOX_BUTTON, false);
+	setGameOption(GameOption::GAME_OPTION_ROOM_TITLE, false);
+
+	auto scene = Scene::create("Othello", "Images/background.png");
+
+
+	for (int y = 0; y < 8; y++) {
+		for (int x = 0; x < 8; x++) {
+			board[y][x] = Object::create("Images/blank.png", scene, 40 + x * 80, 40 + y * 80);
+			board[y][x]->setOnMouseCallback([&, x, y](ObjectPtr object, int, int, MouseAction action)->bool {
+				if (turn == Turn::BLACK) {
+					setState(x, y, State::BLACK);
+					turn = Turn::WHITE;
+				}
+				else {
+					setState(x, y, State::WHITE);
+					turn = Turn::BLACK;
+				}
+				setPossible();
+
+
+				return true;
+				});
+			board_state[y][x] = State::BLANK;
+		}
+	}
+
+	setState(3, 3, State::BLACK);
+	setState(4, 4, State::BLACK);
+	setState(3, 4, State::WHITE);
+	setState(4, 3, State::WHITE);
+
+	setPossible();
+
+	startGame(scene);
+
+
+}
